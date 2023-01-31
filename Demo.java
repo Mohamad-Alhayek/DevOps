8380e9711d5e014b0a72c6dfc8d9f2a123082e29
Mohamad Alhayek <219110268@psu.edu.sa>
2023-01-31 10:52:53 +0300

    <marker message>

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class demo
+{
+       public static void main(string[] args)
+      {
+            System.out.println("Welcome to java");
+            System.out.println("Welcome to DevOps");
+            System.out.println("Git is fun");
+      }
+
+}
