7353ef18f8cfa00cdf362e7e50299ca6de451c84
Mohamad Alhayek <219110268@psu.edu.sa>
2023-02-12 20:42:52 +0300

    Pushing jenkinswithgit.java

--- jenkinswithgit.java
+++ jenkinswithgit.java
@@ -7,10 +7,11 @@ public class jenkinswithgit
 {
 	public static void main(STRING[] args) {
 		int charPerLine=0
-		for (int i = 33; i <256; i++, charPerLine++)
+		for (int i = 20; i <256; i++, charPerLine++)
                         {
                              System.out.print((char)i+((charPerLine%10==0)?"\n":"\t"));
 		     System.out.print();
+                             System.out.print();
                         }
 	}
 }
